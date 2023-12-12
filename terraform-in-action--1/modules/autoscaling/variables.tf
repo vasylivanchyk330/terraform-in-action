bf5522f49f30a487e875969fbe329b0c05c96db5
vasylivanchyk330 <60069876+vasylivanchyk330@users.noreply.github.com>
2023-12-12 12:10:04 +0100

    added refactored version

---
+++ terraform-in-action--1/modules/autoscaling/variables.tf
@@ -0,0 +1,25 @@
+variable "namespace" {
+    type = string
+}
+variable "vpc" {
+    type = any
+}
+variable "sg" {
+    type = any
+}
+
+variable "ssh_keypair" {
+ type = string
+}
+
+variable "db_config" {
+    type = object(
+        {
+            user = string
+            password = string
+            database = string
+            hostname = string
+            port = string
+        }
+    )
+}
---
+++ terraform-in-action--1/modules/networking/variables.tf
@@ -0,0 +1,4 @@
+variable "namespace" {
+    description = "The project namespace to use for unique resource naming"
+    type = string
+}
